84d5dc2e3de807f8dbe88fd6821cbe43dd34f79b
VivianG <wvw4we@virginia.edu>
2024-03-23 23:56:33 -0400

    tried to fix the post request

--- compPal/views.py
+++ compPal/views.py
@@ -12,24 +12,25 @@ from .forms import SurveyForm  # Assuming you have a form class for validation
 def submit_survey(request):
     form = SurveyForm(request.POST)
     if form.is_valid():
-        memory_size = form.cleaned_data['memorySize']
+        import json
+        post_data = json.loads(request.body.decode("utf-8"))
+        memory_size = form.get('memorySize')
+        print(post_data)
         processor = form.cleaned_data['processor']
         #TODO
-        print(memory_size)
         print(processor)
         # Example: Perform some logic based on selected options
-        recommendation = make_recommendation(memory_size, processor)
+        #recommendation = make_recommendation(memory_size, processor)
 
         response_data = {
             'success': True,
             'message': 'Thank you for your submission!',
-            'recommendation': recommendation,  # Include additional data based on form input
+            #'recommendation': recommendation,  # Include additional data based on form input
         }
         return JsonResponse(response_data)
     else:
         return JsonResponse({'success': False, 'errors': form.errors}, status=400)
     
-    return render(request, "name.html", {"form": form})
     
 def make_recommendation(memory_size, processor):
     return scrape_best_buy_laptops(memory_size, processor) 
